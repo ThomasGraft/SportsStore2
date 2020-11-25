002678408e0603e09f922c5bd09a8ecd6c0fb39b
Thomas Graft <33382058+ThomasGraft@users.noreply.github.com>
2020-11-24 19:19:02 -0500

    Add project files.

---
+++ ClientApp/src/app/models/repository.ts
@@ -0,0 +1,27 @@
+import { Product } from "./product.model";
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable()
+export class Repository {
+
+  productData: Product;
+
+  constructor(private http: HttpClient) {
+    this.getProduct(1);
+    //this.product = JSON.parse(document.getElementById("data").textContent);
+  }
+
+  getProduct(id: number) {
+    this.http.get<Product>("/api/products/" + id)
+      .subscribe(p => {
+        this.productData = p;
+        console.log("Product Data Received");
+      });
+  }
+
+  get product(): Product {
+    console.log("Product Data Requested");
+    return this.productData;
+  }
+}
